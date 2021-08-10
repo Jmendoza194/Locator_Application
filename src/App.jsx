393ea7705fc208aeccf5a1efb23620d176ac71fa
Jose Mendoza <joseemendoza194@gmail.com>
2021-08-10 16:10:26 -0700

    Added Grommet, axios and styled-components. Also made empty file components to be filled soon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Grommet } from "grommet";
+
+const App = () => {
+  return (
+    <Grommet plain>
+      <div>App</div>
+    </Grommet>
+  );
+};
+
+export default App;
