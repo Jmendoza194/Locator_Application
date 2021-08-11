dceb542fbb0e1f0757adc3ca7b50f31a15173454
Jose Mendoza <joseemendoza194@gmail.com>
2021-08-10 18:16:43 -0700

    added media queries to support mobile and tablet

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
+import { IoLocationOutline } from "react-icons/io5";
+import { FaBars } from "react-icons/fa";
 import { Header, Text, Image, Box } from "grommet";
+import styled from "styled-components";
 
-import rioLogo from "../../assets/rioseo-logo.png";
+import rioDesktopLogo from "../../assets/rioseo-logo.png";
+import rioMobileLogo from "../../assets/rioseo-logo-mobile.png";
 import locationIcon from "../../assets/location-icon.png";
+import breakpoint from "../../styles/breakpoint";
 
 const HeaderComponent = () => {
   return (
-    <Header>
-      <Image src={rioLogo} alt="rio seo logo" />
-      <Box gap="large" direction="row">
+    <ResponsiveHeader gap="none">
+      <Bar />
+      <DesktopLogo src={rioDesktopLogo} alt="rio seo logo" />
+      <MobileLogo src={rioMobileLogo} alt="rio seo mobile logo" />
+      <DesktopHeaderText gap="large" direction="row">
         <Text>Menu</Text>
         <Box direction="row" gap="xsmall">
           <Box height="20px">
@@ -17,9 +24,65 @@ const HeaderComponent = () => {
           </Box>
           <Text>Truck Locator</Text>
         </Box>
-      </Box>
-    </Header>
+      </DesktopHeaderText>
+      <LocationIcon />
+    </ResponsiveHeader>
   );
 };
 
 export default HeaderComponent;
+
+const ResponsiveHeader = styled(Header)`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  width: 100%;
+  @media only screen and (${breakpoint.device.sm}) {
+    padding: 0.5rem 0;
+    background-color: #444444;
+  }
+`;
+
+const DesktopLogo = styled(Image)`
+  @media only screen and (${breakpoint.device.sm}) {
+    display: none;
+  }
+`;
+
+const MobileLogo = styled(Image)`
+  display: none;
+  @media only screen and (${breakpoint.device.sm}) {
+    align-self: center;
+    display: block;
+  }
+`;
+
+const DesktopHeaderText = styled(Box)`
+  @media only screen and (${breakpoint.device.sm}) {
+    display: none;
+  }
+`;
+
+const LocationIcon = styled(IoLocationOutline)`
+  display: none;
+  @media only screen and (${breakpoint.device.sm}) {
+    display: block;
+    color: white;
+    font-size: 25px;
+    margin-right: 1rem;
+  }
+`;
+
+const Bar = styled(FaBars)`
+  display: none;
+  @media only screen and (${breakpoint.device.sm}) {
+    display: block;
+    color: white;
+    font-size: 25px;
+    margin-left: 1rem;
+
+    &:hover{
+      cursor: pointer;
+    }
+  }
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 
 import { Box } from "grommet";
+import styled from "styled-components";
 
 import Header from "../components/Header/Header.component";
+import breakpoint from "../styles/breakpoint";
 
 const Home = () => {
   return (
-    <Box margin={{ left: "25rem", right: "25rem", top: "1.5rem" }}>
+    <AppContainer>
       <Header />
-    </Box>
+    </AppContainer>
   );
 };
 
 export default Home;
+
+const AppContainer = styled(Box)`
+  margin: 1.5rem 17rem 0 17rem;
+  @media only screen and (${breakpoint.device.lg}) {
+    margin: 1.5rem 10rem 0 10rem;
+  }
+
+  @media only screen and (${breakpoint.device.sm}) {
+    margin: 0;
+  }
+`;
